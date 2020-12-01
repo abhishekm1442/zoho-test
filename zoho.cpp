6b5f9219fdd35adf26ea966383b912dca7d2dfeb
abhishekm1442 <75289896+abhishekm1442@users.noreply.github.com>
2020-12-01 10:08:05 +0530

    Update zoho.cpp

--- zoho.cpp
+++ zoho.cpp
@@ -1,9 +1,32 @@
 #include<bits/stdc++.h>
 
+#define ll long long 
 
 using namespace std ;
 
+void solve()
+{
+	int n ;
+	cin>>n;
+	int k = 2*n-2 ;
+	int num = 1;
+	for(int i=0 ; i<n ; i++)
+	{
+		for(int j=0 ; j<k ; j++)
+			cout<<" ";
+		k-=2 ;
+		for(int j=0 ; j<=i ; j++)
+		{
+			cout<<num<<" ";
+			num++ ; 
+		}
+		cout<<"\n";
+	}
+}
+
 int main()
 {
-
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	solve() ;
 }
